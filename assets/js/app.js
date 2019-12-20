36def424234b10727c17b1ec2fe19bfeabf979af
jstiven01 <stiven10@gmail.com>
2019-12-20 13:12:55 -0500

    Creating Module gameBoard and function setGame

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,34 @@ const Player = (name, token) => {
 
   return { getPlayerName, getPlayerToken };
 };
+
+// Module Gameboard
+const gameBoard = (() => {
+
+  let board;
+  let currentPlayer;
+  let turnX;
+  let turnO;
+  let endRound;
+
+  const setGame = (player) => {
+    turnX = 'X';
+    turnO = 'O';
+    endRound = false;
+    currentPlayer = player;
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  }
+
+  const getCurrentPlayer = ()=> {
+    return currentPlayer;
+  }
+
+  return {
+    setGame, getCurrentPlayer
+  }
+
+})();
+
+playerX = Player('amanda', 'X');
+gameBoard.setGame(playerX);
+console.log('Hey..',gameBoard.getCurrentPlayer().getPlayerName());
