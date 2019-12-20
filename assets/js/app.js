149394bacbc7e54815919a0857d8f4ca53a95b20
vieiramanda11 <amanda_vieira@outlook.com>
2019-12-20 15:19:09 -0300

    create winner conditions for board module

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,29 @@ const gameBoard = (() => {
     return currentPlayer;
   }
 
+  const winHorizontalCondition = () => {
+    if (board[0] === board[1] && board[0] === board[2] ||
+        board[3] === board[4] && board[3] === board[5] ||
+        board[6] === board[7] && board[6] === board[8]) {
+          return true;
+        }
+  };
+
+  const winVerticalCondition = () => {
+    if (board[0] == board[3] && board[0] == board[6] ||
+        board[1] == board[4] && board[1] == board[7] ||
+        board[2] == board[5] && board[2] == board[8]) {
+          return true;
+       }
+  };
+
+  const winDiagonalCondition = () => {
+    if (board[0] == board[4] && board[0] == board[8] ||
+        board[2] == board[4] && board[2] == board[6]) {
+          return true;
+        }
+  };
+
   return {
     setGame, getCurrentPlayer
   }
