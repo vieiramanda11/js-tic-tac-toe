7c1c4fc9f9d85c554eaceb579348433d8a662520
jstiven01 <stiven10@gmail.com>
2019-12-23 10:49:46 -0500

    Fixing Pull request errors

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,38 +24,6 @@ const gameBoard = (() => {
 
   const getCurrentPlayer = () => currentPlayer;
 
-  const winHorizontalCondition = () => {
-    if (
-      (board[0] === board[1] && board[0] === board[2])
-      || (board[3] === board[4] && board[3] === board[5])
-      || (board[6] === board[7] && board[6] === board[8])
-    ) {
-      return true;
-    }
-    return false;
-  };
-
-  const winVerticalCondition = () => {
-    if (
-      (board[0] === board[3] && board[0] === board[6])
-      || (board[1] === board[4] && board[1] === board[7])
-      || (board[2] === board[5] && board[2] === board[8])
-    ) {
-      return true;
-    }
-    return false;
-  };
-
-  const winDiagonalCondition = () => {
-    if (
-      (board[0] === board[4] && board[0] === board[8])
-      || (board[2] === board[4] && board[2] === board[6])
-    ) {
-      return true;
-    }
-    return false;
-  };
-
   const players = (first, second) => {
     firstPlayer = Player(first, turnX);
     secondPlayer = Player(second, turnO);
@@ -90,21 +58,22 @@ const gameBoard = (() => {
   };
 
   const win = () => {
-    if (
-      winDiagonalCondition()
-      || winHorizontalCondition()
-      || winVerticalCondition()
-    ) {
-      return true;
+    let lastToken = currentPlayer.getPlayerToken()==='X' ? 'O' : 'X';
+    const winnerCases = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    for (let i = 0; i < winnerCases.length; i += 1){
+      if (board[winnerCases[i][0]] === lastToken && 
+          board[winnerCases[i][1]] === lastToken && 
+          board[winnerCases[i][2]] === lastToken ) {
+          return true;
+      }
     }
     return false;
   };
 
   const winner = () => {
-    if (currentPlayer === firstPlayer) {
-      return secondPlayer.getPlayerName();
-    }
-    return firstPlayer.getPlayerName();
+    let lastPlayerName = currentPlayer === firstPlayer ? secondPlayer.getPlayerName() : firstPlayer.getPlayerName();
+
+    return lastPlayerName;
   };
 
   const boardEmpty = () => {
